4524401889aa3e6d4e7ddfc64684545ebec37150
cashcoder88 <cashglobe@gmail.com>
2019-06-11 16:45:34 -0600

    Crappy bug fixed in QandA

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-// const usersRouter = require('../users/users-router.js')
-// const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js')
+const authRouter = require('../auth/auth-router.js');
 
 const server = express();
 
@@ -11,11 +11,14 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter)
+
+
 server.get('/', (req, res) => {
     res.json("It's alive!");
 });
 
-// server.use('/api/users', usersRouter)
-// server.use('/api/auth', authRouter)
+
 
 module.exports = server; 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,11 +3,10 @@ const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model');
 
-
 router.post('/register', (req, res) => {
     let user = req.body;
-
-    const hash = bcrypt.hashSync(user.password, 8);
+    console.log(user)
+    const hash = bcrypt.hashSync(user.password, 10);
 
     user.password = hash;
 
@@ -24,7 +23,7 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
     let {username, password} = req.body;
-
+    console.log(req.body)
     Users.findBy({username})
     .first()
     .then(user => {
