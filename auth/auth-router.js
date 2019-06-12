cbb61a4760dc28bfd34a970bcfcef149678950c0
cashcoder88 <cashglobe@gmail.com>
2019-06-11 18:26:20 -0600

    added dependencies

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,7 @@ const Users = require('../users/users-model');
 
 router.post('/register', (req, res) => {
     let user = req.body;
-    console.log(user)
+    console.log('user', user)
     const hash = bcrypt.hashSync(user.password, 10);
 
     user.password = hash;
@@ -22,24 +22,20 @@ router.post('/register', (req, res) => {
 
 
 router.post('/login', (req, res) => {
-    let {username, password} = req.body;
+    let { username, password } = req.body;
     console.log(req.body)
-    Users.findBy({username})
-    .first()
-    .then(user => {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-            res.status(200).json({
-                message: `Welcome ${user.username}`
-            })
+          res.status(200).json({ message: `Welcome ${user.username}!` });
         } else {
-            res.status(401).json({
-                errorMessage: 'You Shall Not Pass'
-            })
+          res.status(401).json({ message: 'Invalid Credentials' });
         }
-    })
-    .catch(error => {
-        res.status(500).json(error)
-    })
-});
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 module.exports = router; 
